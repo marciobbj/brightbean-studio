b8a4e6992e13facc8cf5d859d4d6d462811cddaf
Jan Schmitz <jan.schmitz@whu.edu>
2026-03-31 12:47:23 +0200

    media library previews

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -32,3 +34,6 @@ urlpatterns = [
     path("organizations/media/", include("apps.media_library.urls_org")),
     path("", include("apps.accounts.urls_root")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
